b8a070600bfa135346ab6e7ea1b8b0c814d88119
argojuni <juniargoprasaja05@gmail.com>
2023-02-23 09:20:33 +0700

    03 Movement

--- Example Game Project/Assets/Script/PlayerMovement.cs
+++ Example Game Project/Assets/Script/PlayerMovement.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     rb.AddForce(0,200,500);
-    // }
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0,0,2000 * Time.deltaTime);
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
