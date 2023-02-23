a32f452368c28911264e09dbd6104fd6839b58e1
argojuni <juniargoprasaja05@gmail.com>
2023-02-23 15:18:49 +0700

    08 Game Over

--- Example Game Project/Assets/Script/FollowPlayer.cs
+++ Example Game Project/Assets/Script/FollowPlayer.cs
@@ -10,5 +10,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         transform.position = player.position + offset;
+        FindObjectOfType<PlayerMovement>(transform);
     }
 }
